580c50f5f88e88a12deab3da8ae892fb95189768
onurjadermark <onurjadermark@gmail.com>
2022-11-29 19:37:38 +0100

    Initial commit

---
+++ Solutions/Utils/GridConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace Solutions.Utils;
+
+public static class GridConstants
+{
+    public static readonly List<(int X, int Y)> Neighbours = new()
+    {
+        (1, 0),
+        (1, 1),
+        (0, 1),
+        (-1, 1),
+        (-1, 0),
+        (-1, -1),
+        (0, -1),
+        (1, -1)
+    };
+}
---
+++ Tests/Day06Test.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using Solutions;
+
+namespace Tests;
+
+[TestFixture]
+public class Day06Test
+{
+    private readonly string[] _sampleInput = @"".Split(Environment.NewLine).ToArray();
+
+    private Day06 GetInstance() => new();
+
+    [Test]
+    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(0);
+
+    [Test]
+    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(0);
+}
