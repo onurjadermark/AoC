d9c7ee034b82838d7ae371aaa5865b6f4d47885f
onurjadermark <onurjadermark@gmail.com>
2022-12-02 07:23:50 +0100

    Refact

--- Solutions/Solutions/2022/Day02.cs
+++ Solutions/Solutions/2022/Day02.cs
@@ -7,24 +7,12 @@ public class Day02
         var score = 0;
         foreach (var line in input)
         {
-            var roundScore = 0;
-            var split = line.Split(" ");
-            var opponent = split[0][0] - 'A';
-            var me = split[1][0] - 'X';
-            
-            roundScore += me + 1;
-            if ((opponent + 1) % 3 == me)
-            {
-                roundScore += 6;
-            }
-            else if (opponent == me)
-            {
-                roundScore += 3;
-            }
-            
-            score += roundScore;
+            var opponent = (Move) ExtractToken(line, 0);
+            var me = (Move) ExtractToken(line, 1);
+            var result = GetResult(opponent, me);
+            score += GetScore(result, me);
         }
-        
+
         return score;
     }
 
@@ -33,23 +21,51 @@ public class Day02
         var score = 0;
         foreach (var line in input)
         {
-            var roundScore = 0;
-            var split = line.Split(" ");
-            var opponent = split[0][0] - 'A';
-            var result = split[1][0] - 'X';
-            
-            roundScore += result * 3;
-            var me = result switch
-            {
-                2 => (opponent + 1) % 3,
-                1 => opponent,
-                _ => (opponent + 2) % 3
-            };
-            roundScore += me + 1;
-
-            score += roundScore;
+            var opponent = (Move) ExtractToken(line, 0);
+            var result = (Result) ExtractToken(line, 1);
+            var me = GetMyMove(result, opponent);
+            score += GetScore(result, me);
         }
-        
+
         return score;
+    }
+
+    private static Move GetMyMove(Result result, Move opponent)
+    {
+        return result switch
+        {
+            Result.Win => (Move) (((int) opponent + 1) % 3),
+            Result.Draw => opponent,
+            Result.Lose => (Move) (((int) opponent + 2) % 3),
+            _ => throw new NotImplementedException()
+        };
+    }
+
+    private static Result GetResult(Move opponent, Move me)
+    {
+        if (((int) opponent + 1) % 3 == (int) me)
+        {
+            return Result.Win;
+        }
+
+        return opponent == me ? Result.Draw : Result.Lose;
+    }
+
+    private static int GetScore(Result result, Move me) => (int) me + 1 + (int) result * 3;
+
+    private static int ExtractToken(string line, int part) => line.Split(" ")[part][0] - (part == 0 ? 'A' : 'X');
+
+    private enum Move
+    {
+        Rock = 0,
+        Paper = 1,
+        Scissor = 2
+    }
+
+    private enum Result
+    {
+        Lose = 0,
+        Draw = 1,
+        Win = 2
     }
 }
