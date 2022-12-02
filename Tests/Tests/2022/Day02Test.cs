f37b3f5f975c70ee171440d627de41e355e2e794
onurjadermark <onurjadermark@gmail.com>
2022-12-02 06:34:22 +0100

    Add day 2 solution

--- Tests/Tests/2022/Day02Test.cs
+++ Tests/Tests/2022/Day02Test.cs
@@ -9,13 +9,15 @@ namespace Tests.Tests._2022;
 [TestFixture]
 public class Day02Test
 {
-    private readonly string[] _sampleInput = @"".Split(Environment.NewLine).ToArray();
+    private readonly string[] _sampleInput = @"A Y
+B X
+C Z".Split(Environment.NewLine).ToArray();
 
     private Day02 GetInstance() => new();
 
     [Test]
-    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(0);
+    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(15);
 
     [Test]
-    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(0);
+    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(12);
 }
