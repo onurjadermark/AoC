f3f701270f2f0028311cb7043bb71acbcacfe24f
onurjadermark <onurjadermark@gmail.com>
2022-12-03 06:29:12 +0100

    Add day 3 solution

--- Tests/Tests/2022/Day03Test.cs
+++ Tests/Tests/2022/Day03Test.cs
@@ -9,13 +9,18 @@ namespace Tests.Tests._2022;
 [TestFixture]
 public class Day03Test
 {
-    private readonly string[] _sampleInput = @"".Split(Environment.NewLine).ToArray();
+    private readonly string[] _sampleInput = @"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw".Split(Environment.NewLine).ToArray();
 
     private Day03 GetInstance() => new();
 
     [Test]
-    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(0);
+    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(157);
 
     [Test]
-    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(0);
+    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(70);
 }
