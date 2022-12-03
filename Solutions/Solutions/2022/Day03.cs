be8c823fd2ca35888e7131db2f63e36560b7ad2a
onurjadermark <onurjadermark@gmail.com>
2022-12-03 06:29:12 +0100

    Add day 3 solution

--- Solutions/Solutions/2022/Day03.cs
+++ Solutions/Solutions/2022/Day03.cs
@@ -4,11 +4,37 @@ public class Day03
 {
     public int Part1(string[] input)
     {
-        return 0;
+        var total = 0;
+        foreach (var line in input)
+        {
+            var first = line.Skip(line.Length / 2);
+            var second = line.Take(line.Length / 2);
+            var common = first.First(x => second.Contains(x));
+            total += GetValue(common);
+        }
+
+        return total;
     }
 
     public int Part2(string[] input)
     {
-        return 0;
+        var total = 0;
+        var group = new List<string>();
+        foreach (var t in input)
+        {
+            group.Add(t);
+            if (group.Count != 3) continue;
+            var common = group[0].First(x => group[1].Contains(x) && group[2].Contains(x));
+            total += GetValue(common);
+            group.Clear();
+        }
+
+        return total;
+    }
+
+    private static int GetValue(char letter)
+    {
+        var result = letter - 'a' + 1;
+        return result <= 0 ? letter - 'A' + 27 : result;
     }
 }
