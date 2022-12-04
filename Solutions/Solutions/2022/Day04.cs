032144c4810e570978ca3c2e048136efc0b77b35
onurjadermark <onurjadermark@gmail.com>
2022-12-04 12:25:37 +0100

    Refact

--- Solutions/Solutions/2022/Day04.cs
+++ Solutions/Solutions/2022/Day04.cs
@@ -4,36 +4,31 @@ public class Day04
 {
     public int Part1(string[] input)
     {
-        return Solve(input, false);
+        return Solve(input, 1);
     }
 
     public int Part2(string[] input)
     {
-        return Solve(input, true);
+        return Solve(input, 2);
     }
 
-    private static int Solve(string[] input, bool isPartTwo)
+    private static int Solve(string[] input, int part)
     {
         var result = 0;
         foreach (var line in input)
         {
-            var sections = line.Split(",");
-            var firstElf = sections[0].Split("-").Select(int.Parse).ToList();
-            var secondElf = sections[1].Split("-").Select(int.Parse).ToList();
-            var firstAssignments = Enumerable.Range(firstElf[0], firstElf[1] - firstElf[0] + 1).ToList();
-            var secondAssignments = Enumerable.Range(secondElf[0], secondElf[1] - secondElf[0] + 1).ToList();
-            if (isPartTwo)
+            var numbers = line.Split('-', ',').Select(int.Parse).ToList();
+            if (part == 1)
             {
-                if (firstAssignments.Any(x => secondAssignments.Contains(x)) ||
-                    secondAssignments.Any(x => firstAssignments.Contains(x)))
+                if ((numbers[0] >= numbers[2] && numbers[1] <= numbers[3]) ||
+                    (numbers[2] >= numbers[0] && numbers[3] <= numbers[1]))
                 {
                     result++;
                 }
             }
             else
             {
-                if (firstAssignments.All(x => secondAssignments.Contains(x)) ||
-                    secondAssignments.All(x => firstAssignments.Contains(x)))
+                if (numbers[1] >= numbers[2] && numbers[0] <= numbers[3])
                 {
                     result++;
                 }
