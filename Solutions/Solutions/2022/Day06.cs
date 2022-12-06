842e9802b45423d728e6eece141aa1ff53bb1c6c
onurjadermark <onurjadermark@gmail.com>
2022-12-06 06:08:16 +0100

    Add day 6 solution

--- Solutions/Solutions/2022/Day06.cs
+++ Solutions/Solutions/2022/Day06.cs
@@ -4,11 +4,31 @@ public class Day06
 {
     public int Part1(string[] input)
     {
-        return 0;
+        return Solve(input, 4);
     }
 
     public int Part2(string[] input)
     {
+        return Solve(input, 14);
+    }
+
+    private static int Solve(string[] input, int length)
+    {
+        var cur = "";
+        foreach (var ch in input[0])
+        {
+            cur += ch;
+            if (cur.Length > length)
+            {
+                cur = cur.Substring(1);
+            }
+
+            if (cur.Length == length && cur.Distinct().Count() == length)
+            {
+                return input[0].IndexOf(cur, StringComparison.Ordinal) + length;
+            }
+        }
+
         return 0;
     }
 }
--- Tests/Tests/2022/Day06Test.cs
+++ Tests/Tests/2022/Day06Test.cs
@@ -9,13 +9,13 @@ namespace Tests.Tests._2022;
 [TestFixture]
 public class Day06Test
 {
-    private readonly string[] _sampleInput = @"".Split(Environment.NewLine).ToArray();
+    private readonly string[] _sampleInput = @"mjqjpqmgbljsphdztnvjfqwrcgsmlb".Split(Environment.NewLine).ToArray();
 
     private Day06 GetInstance() => new();
 
     [Test]
-    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(0);
+    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(7);
 
     [Test]
-    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(0);
+    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(19);
 }
