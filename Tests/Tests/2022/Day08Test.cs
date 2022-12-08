9cdb072a2b06d25c4df81663b3baaf1e60690795
onurjadermark <onurjadermark@gmail.com>
2022-12-08 07:48:37 +0100

    Add day 8 solution

--- Tests/Tests/2022/Day08Test.cs
+++ Tests/Tests/2022/Day08Test.cs
@@ -9,13 +9,17 @@ namespace Tests.Tests._2022;
 [TestFixture]
 public class Day08Test
 {
-    private readonly string[] _sampleInput = @"".Split(Environment.NewLine).ToArray();
+    private readonly string[] _sampleInput = @"30373
+25512
+65332
+33549
+35390".Split(Environment.NewLine).ToArray();
 
     private Day08 GetInstance() => new();
 
     [Test]
-    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(0);
+    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(21);
 
     [Test]
-    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(0);
+    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(8);
 }
