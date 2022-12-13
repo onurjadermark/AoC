e866597bf24a2bd1ac227dfbccc9b94f4958463e
onurjadermark <onurjadermark@gmail.com>
2022-12-13 07:14:58 +0100

    Refact

--- Solutions/Solutions/2022/Day13.cs
+++ Solutions/Solutions/2022/Day13.cs
@@ -8,65 +8,75 @@ public class Day13
     {
         var result = 0;
         var index = 1;
-        
-        for (var i = 0; i < input.Length; i+=3)
+
+        for (var i = 0; i < input.Length; i += 3)
         {
             var first = input[i];
             var second = input[i + 1];
-            if (Compare(JToken.Parse(first), JToken.Parse(second)) < 0) result += index;
+            if (new Packet(JToken.Parse(first)).CompareTo(new Packet(JToken.Parse(second))) < 0)
+            {
+                result += index;
+            }
+
             index++;
         }
-        
+
         return result;
     }
 
     public int Part2(string[] input)
     {
         var dividerPackets = new[] {"[[2]]", "[[6]]"}.Select(JToken.Parse).ToList();
-        var packets = input.Where(x => !string.IsNullOrEmpty(x)).Select(JToken.Parse).Concat(dividerPackets).ToList();
+        var packets = input.Where(x => !string.IsNullOrEmpty(x)).Select(JToken.Parse).Concat(dividerPackets)
+            .Select(x => new Packet(x)).ToList();
 
-        for (var i = 0; i < packets.Count; i++)
-        {
-            for (var j = i + 1; j < packets.Count; j++)
-            {
-                if (Compare(packets[i], packets[j]) > 0)
-                {
-                    (packets[j], packets[i]) = (packets[i], packets[j]);
-                }
-            }
-        }
+        packets.Sort();
 
-        return dividerPackets.Select(x => packets.IndexOf(x) + 1).Aggregate((x, y) => x * y);
+        return packets.Where(x => dividerPackets.Contains(x.Token)).Select(x => packets.IndexOf(x) + 1)
+            .Aggregate((x, y) => x * y);
     }
 
-    private static int Compare(JToken first, JToken second)
+    private class Packet : IComparable<Packet>
     {
-        if (first is JValue && second is JValue)
+        public Packet(JToken token)
         {
-            return first.Value<int>().CompareTo(second.Value<int>());
-        }
-        
-        if (first is not JArray)
-        {
-            first = JToken.Parse($"[{first}]");
+            Token = token;
         }
 
-        if (second is not JArray)
-        {
-            second = JToken.Parse($"[{second}]");
-        }
+        public JToken Token { get; }
 
-        for (var i = 0; i < Math.Max(first.Count(), second.Count()); i++)
+        public int CompareTo(Packet? other)
         {
-            if (first.Count() == i) return -1;
-            var curFirst = first[i];
-            if (second.Count() == i) return 1;
-            var curSecond = second[i];
-            var comparison = Compare(curFirst, curSecond);
-            if (comparison == 0) continue;
-            return comparison;
-        }
+            var first = Token;
+            var second = other!.Token;
 
-        return 0;
+            if (first is JValue && second is JValue)
+            {
+                return first.Value<int>().CompareTo(second.Value<int>());
+            }
+
+            if (first is not JArray)
+            {
+                first = JToken.Parse($"[{first}]");
+            }
+
+            if (second is not JArray)
+            {
+                second = JToken.Parse($"[{second}]");
+            }
+
+            for (var i = 0; i < Math.Max(first.Count(), second.Count()); i++)
+            {
+                if (first.Count() == i) return -1;
+                var curFirst = first[i];
+                if (second.Count() == i) return 1;
+                var curSecond = second[i];
+                var comparison = new Packet(curFirst).CompareTo(new Packet(curSecond));
+                if (comparison == 0) continue;
+                return comparison;
+            }
+
+            return 0;
+        }
     }
 }
