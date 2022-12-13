de74f9afdc566dfbe530ea76a967402641e55f1f
onurjadermark <onurjadermark@gmail.com>
2022-12-13 07:02:10 +0100

    Add day 13 solution

--- Solutions/Solutions/2022/Day13.cs
+++ Solutions/Solutions/2022/Day13.cs
@@ -1,14 +1,72 @@
-﻿namespace Solutions.Solutions._2022;
+﻿using Newtonsoft.Json.Linq;
+
+namespace Solutions.Solutions._2022;
 
 public class Day13
 {
     public int Part1(string[] input)
     {
-        return 0;
+        var result = 0;
+        var index = 1;
+        
+        for (var i = 0; i < input.Length; i+=3)
+        {
+            var first = input[i];
+            var second = input[i + 1];
+            if (Compare(JToken.Parse(first), JToken.Parse(second)) < 0) result += index;
+            index++;
+        }
+        
+        return result;
     }
 
     public int Part2(string[] input)
     {
+        var dividerPackets = new[] {"[[2]]", "[[6]]"}.Select(JToken.Parse).ToList();
+        var packets = input.Where(x => !string.IsNullOrEmpty(x)).Select(JToken.Parse).Concat(dividerPackets).ToList();
+
+        for (var i = 0; i < packets.Count; i++)
+        {
+            for (var j = i + 1; j < packets.Count; j++)
+            {
+                if (Compare(packets[i], packets[j]) > 0)
+                {
+                    (packets[j], packets[i]) = (packets[i], packets[j]);
+                }
+            }
+        }
+
+        return dividerPackets.Select(x => packets.IndexOf(x) + 1).Aggregate((x, y) => x * y);
+    }
+
+    private static int Compare(JToken first, JToken second)
+    {
+        if (first is JValue && second is JValue)
+        {
+            return first.Value<int>().CompareTo(second.Value<int>());
+        }
+        
+        if (first is not JArray)
+        {
+            first = JToken.Parse($"[{first}]");
+        }
+
+        if (second is not JArray)
+        {
+            second = JToken.Parse($"[{second}]");
+        }
+
+        for (var i = 0; i < Math.Max(first.Count(), second.Count()); i++)
+        {
+            if (first.Count() == i) return -1;
+            var curFirst = first[i];
+            if (second.Count() == i) return 1;
+            var curSecond = second[i];
+            var comparison = Compare(curFirst, curSecond);
+            if (comparison == 0) continue;
+            return comparison;
+        }
+
         return 0;
     }
 }
--- Tests/Tests/2022/Day13Test.cs
+++ Tests/Tests/2022/Day13Test.cs
@@ -9,13 +9,35 @@ namespace Tests.Tests._2022;
 [TestFixture]
 public class Day13Test
 {
-    private readonly string[] _sampleInput = @"".Split(Environment.NewLine).ToArray();
+    private readonly string[] _sampleInput = @"[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]".Split(Environment.NewLine).ToArray();
 
     private Day13 GetInstance() => new();
 
     [Test]
-    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(0);
+    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(13);
 
     [Test]
-    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(0);
+    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(140);
 }
