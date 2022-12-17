fbabe9406b5907c790deedb3ab752294135f6441
onurjadermark <onurjadermark@gmail.com>
2022-12-17 10:24:29 +0100

    Add day 17 solution

--- Tests/Tests/2022/Day17Test.cs
+++ Tests/Tests/2022/Day17Test.cs
@@ -9,13 +9,13 @@ namespace Tests.Tests._2022;
 [TestFixture]
 public class Day17Test
 {
-    private readonly string[] _sampleInput = @"".Split(Environment.NewLine).ToArray();
+    private readonly string[] _sampleInput = @">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>".Split(Environment.NewLine).ToArray();
 
     private Day17 GetInstance() => new();
 
     [Test]
-    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(0);
+    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(3068);
 
     [Test]
-    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(0);
+    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(1514285714288);
 }
