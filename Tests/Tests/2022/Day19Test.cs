a2042ff1f299c5b21a5597463cd608d6217e303a
onurjadermark <onurjadermark@gmail.com>
2022-12-19 11:45:57 +0100

    Add day 19 solution

--- Tests/Tests/2022/Day19Test.cs
+++ Tests/Tests/2022/Day19Test.cs
@@ -9,13 +9,14 @@ namespace Tests.Tests._2022;
 [TestFixture]
 public class Day19Test
 {
-    private readonly string[] _sampleInput = @"".Split(Environment.NewLine).ToArray();
+    private readonly string[] _sampleInput = @"Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.".Split(Environment.NewLine).ToArray();
 
     private Day19 GetInstance() => new();
 
     [Test]
-    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(0);
+    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(33);
 
     [Test]
-    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(0);
+    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(3472);
 }
