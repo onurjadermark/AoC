8703260fe21a423d0cc9240a1570fbadf970d70d
onurjadermark <onurjadermark@gmail.com>
2022-12-25 15:18:38 +0100

    Add solution for day 25

--- Tests/Tests/2022/Day25Test.cs
+++ Tests/Tests/2022/Day25Test.cs
@@ -9,13 +9,22 @@ namespace Tests.Tests._2022;
 [TestFixture]
 public class Day25Test
 {
-    private readonly string[] _sampleInput = @"".Split(Environment.NewLine).ToArray();
+    private readonly string[] _sampleInput = @"1=-0-2
+12111
+2=0=
+21
+2=01
+111
+20012
+112
+1=-1=
+1-12
+12
+1=
+122".Split(Environment.NewLine).ToArray();
 
     private Day25 GetInstance() => new();
 
     [Test]
-    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(0);
-
-    [Test]
-    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(0);
+    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be("2=-1=0");
 }
