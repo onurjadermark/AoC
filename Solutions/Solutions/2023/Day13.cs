0ba7356b617f58b7cb9791d5668bc9e3a1436e33
onurjadermark <onurjadermark@gmail.com>
2023-11-27 19:01:35 +0100

    Add placeholder files for 2023

---
+++ Solutions/Solutions/2023/Day13.cs
@@ -0,0 +1,14 @@
+﻿namespace Solutions.Solutions._2023;
+
+public class Day13
+{
+    public int Part1(string[] input)
+    {
+        return 0;
+    }
+
+    public int Part2(string[] input)
+    {
+        return 0;
+    }
+}
