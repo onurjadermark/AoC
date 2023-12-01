5a96f74f56caa73ca1e6e2c7bd0ca64824e208c8
onurjadermark <onurjadermark@gmail.com>
2023-12-01 07:03:53 +0100

    Add solution for day 1

--- Solutions/Solutions/2023/Day01.cs
+++ Solutions/Solutions/2023/Day01.cs
@@ -2,13 +2,45 @@
 
 public class Day01
 {
+    private static readonly string[] Digits = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
     public int Part1(string[] input)
     {
-        return 0;
+        return Solve(input, 1);
     }
 
     public int Part2(string[] input)
     {
-        return 0;
+        return Solve(input, 2);
+    }
+
+    private int Solve(string[] input, int part)
+    {
+        var sum = 0;
+
+        foreach (var line in input)
+        {
+            var digits = new List<int>();
+
+            for (var i = 0; i < line.Length; i++)
+            {
+                if (char.IsDigit(line[i]))
+                {
+                    digits.Add(line[i] - '0');
+                }
+
+                if (part == 1) continue;
+
+                var digit = Array.IndexOf(Digits, Digits.FirstOrDefault(x => line.Substring(i, Math.Min(x.Length, line.Length - i)) == x));
+                if (digit != -1)
+                {
+                    digits.Add(digit);
+                }
+            }
+
+            sum += digits.First() * 10 + digits.Last();
+        }
+
+        return sum;
     }
 }
