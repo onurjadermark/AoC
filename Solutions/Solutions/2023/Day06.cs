3f666b287964afc43754e5011da3c40b5d73609f
onurjadermark <onurjadermark@gmail.com>
2023-12-06 06:20:58 +0100

    Add solution for day 6

--- Solutions/Solutions/2023/Day06.cs
+++ Solutions/Solutions/2023/Day06.cs
@@ -4,11 +4,40 @@ public class Day06
 {
     public int Part1(string[] input)
     {
-        return 0;
+        return Solve(input, 1);
     }
 
     public int Part2(string[] input)
     {
-        return 0;
+        return Solve(input, 2);
+    }
+
+    private static int Solve(string[] input, int part)
+    {
+        var times = input[0].Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(long.Parse).ToArray();
+        var distances = input[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(long.Parse).ToArray();
+
+        if (part == 2)
+        {
+            times = new[] {long.Parse(string.Join("", times))};
+            distances = new[] {long.Parse(string.Join("", distances))};
+        }
+
+        var counts = new List<int>();
+
+        for (var i = 0; i < times.Length; i++)
+        {
+            counts.Add((int) GetCount(times[i], distances[i]));
+        }
+
+        return counts.Where(x => x != 0).Aggregate((x, y) => x * y);
+    }
+
+    private static long GetCount(long time, long distance)
+    {
+        var disc = time * time - 4 * distance;
+        var min = (long) (time - Math.Sqrt(disc)) / 2 + 1;
+        var max = time - min;
+        return max - min + 1;
     }
 }
--- Tests/Tests/2023/Day06Test.cs
+++ Tests/Tests/2023/Day06Test.cs
@@ -9,13 +9,14 @@ namespace Tests.Tests._2023;
 [TestFixture]
 public class Day06Test
 {
-    private readonly string[] _sampleInput = @"".Split(Environment.NewLine).ToArray();
+    private readonly string[] _sampleInput = @"Time:      7  15   30
+Distance:  9  40  200".Split(Environment.NewLine).ToArray();
 
     private Day06 GetInstance() => new();
 
     [Test]
-    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(0);
+    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(288);
 
     [Test]
-    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(0);
+    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(71503);
 }
