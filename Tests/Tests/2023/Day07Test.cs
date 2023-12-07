3063553d9036f0797a702dd235d74f5499f60b81
onurjadermark <onurjadermark@gmail.com>
2023-12-07 08:51:22 +0100

    Add solution for day 7

--- Tests/Tests/2023/Day07Test.cs
+++ Tests/Tests/2023/Day07Test.cs
@@ -9,13 +9,17 @@ namespace Tests.Tests._2023;
 [TestFixture]
 public class Day07Test
 {
-    private readonly string[] _sampleInput = @"".Split(Environment.NewLine).ToArray();
+    private readonly string[] _sampleInput = @"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483".Split(Environment.NewLine).ToArray();
 
     private Day07 GetInstance() => new();
 
     [Test]
-    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(0);
+    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(6440);
 
     [Test]
-    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(0);
+    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(5905);
 }
