568415a39f79c9168751215244a9258d5096ad2a
onurjadermark <onurjadermark@gmail.com>
2023-12-08 08:15:38 +0100

    Add solution for day 8

--- Solutions/Solutions/2023/Day08.cs
+++ Solutions/Solutions/2023/Day08.cs
@@ -2,13 +2,47 @@
 
 public class Day08
 {
-    public int Part1(string[] input)
+    public long Part1(string[] input)
     {
-        return 0;
+        return Solve(input, 1);
     }
 
-    public int Part2(string[] input)
+    public long Part2(string[] input)
     {
-        return 0;
+        return Solve(input, 2);
     }
+
+    private static long Solve(string[] input, int part)
+    {
+        var moves = input[0];
+        var nodes = input.Skip(2).Select(x => x.Split(',', '(', ')', '=').Select(y => y.Trim()).Where(y => !string.IsNullOrWhiteSpace(y)).ToList())
+            .ToDictionary(x => x[0], x => x.Skip(1).ToList());
+
+        var steps = 0;
+        var curNodes = nodes.Select(x => x.Key).Where(x => part == 1 ? x == "AAA" : x.Last() == 'A').ToList();
+        var indexes = new long?[curNodes.Count];
+        
+        while (indexes.Any(x => !x.HasValue))
+        {
+            var move = moves[steps % moves.Length];
+            curNodes = curNodes.Select(x => nodes[x].ElementAt(move == 'L' ? 0 : 1)).ToList();
+            steps++;
+
+            for (var i = 0; i < curNodes.Count; i++)
+            {
+                if (curNodes[i].Last() == 'Z' && !indexes[i].HasValue)
+                {
+                    indexes[i] = steps;
+                }
+            }
+        }
+        
+        return LCM(indexes.Select(x => x!.Value).ToArray());
+    }
+
+    private static long GCD(long a, long b) => b == 0 ? a : GCD(b, a % b);
+
+    private static long LCM(long a, long b) => a / GCD(a, b) * b;
+
+    private static long LCM(IEnumerable<long> values) => values.Aggregate(LCM);
 }
--- Tests/Tests/2023/Day08Test.cs
+++ Tests/Tests/2023/Day08Test.cs
@@ -9,13 +9,41 @@ namespace Tests.Tests._2023;
 [TestFixture]
 public class Day08Test
 {
-    private readonly string[] _sampleInput = @"".Split(Environment.NewLine).ToArray();
+    private readonly string[] _sampleInput1 = @"RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)".Split(Environment.NewLine).ToArray();
+    
+    private readonly string[] _sampleInput2 = @"LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)".Split(Environment.NewLine).ToArray();
+    
+    private readonly string[] _sampleInput3 = @"LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)".Split(Environment.NewLine).ToArray();
 
     private Day08 GetInstance() => new();
 
     [Test]
-    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(0);
+    public void Part1A() => GetInstance().Part1(_sampleInput1).Should().Be(2);
 
     [Test]
-    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(0);
+    public void Part1B() => GetInstance().Part1(_sampleInput2).Should().Be(6);
+
+    [Test]
+    public void Part2() => GetInstance().Part2(_sampleInput3).Should().Be(6);
 }
