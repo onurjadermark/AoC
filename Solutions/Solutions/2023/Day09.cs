1100dbed8e74d7df7b78cb8a63236fe86fc964a2
onurjadermark <onurjadermark@gmail.com>
2023-12-09 10:19:33 +0100

    Add solution for day 9

--- Solutions/Solutions/2023/Day09.cs
+++ Solutions/Solutions/2023/Day09.cs
@@ -2,13 +2,32 @@
 
 public class Day09
 {
-    public int Part1(string[] input)
+    public long Part1(string[] input)
     {
-        return 0;
+        return Solve(input, 1);
     }
 
-    public int Part2(string[] input)
+    public long Part2(string[] input)
     {
-        return 0;
+        return Solve(input, 2);
+    }
+
+    private long Solve(string[] input, int part)
+    {
+        var sum = 0L;
+        foreach (var numbers in input.Select(x => x.Split(' ').Select(long.Parse).ToArray()).ToArray())
+        {
+            var cur = numbers.ToList();
+            var diffs = new List<List<long>> {cur};
+            while (cur.Any(x => x != 0))
+            {
+                cur = cur.Zip(cur.Skip(1)).Select(x => x.Second - x.First).ToList();
+                diffs.Add(cur);
+            }
+
+            sum += diffs.Select((x, i) => part == 1 ? x.Last() : x.First() * (i % 2 == 0 ? 1 : -1)).Sum();
+        }
+
+        return sum;
     }
 }
--- Tests/Tests/2023/Day09Test.cs
+++ Tests/Tests/2023/Day09Test.cs
@@ -9,13 +9,15 @@ namespace Tests.Tests._2023;
 [TestFixture]
 public class Day09Test
 {
-    private readonly string[] _sampleInput = @"".Split(Environment.NewLine).ToArray();
+    private readonly string[] _sampleInput = @"0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45".Split(Environment.NewLine).ToArray();
 
     private Day09 GetInstance() => new();
 
     [Test]
-    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(0);
+    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(114);
 
     [Test]
-    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(0);
+    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(2);
 }
