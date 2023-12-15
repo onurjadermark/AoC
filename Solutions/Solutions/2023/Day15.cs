da66fa4ca07a7747b02c2653ed209f5bc2251dff
onurjadermark <onurjadermark@gmail.com>
2023-12-15 09:31:32 +0100

    Add solution for day 15

--- Solutions/Solutions/2023/Day15.cs
+++ Solutions/Solutions/2023/Day15.cs
@@ -4,11 +4,60 @@ public class Day15
 {
     public int Part1(string[] input)
     {
-        return 0;
+        return Solve(input, 1);
     }
 
     public int Part2(string[] input)
     {
-        return 0;
+        return Solve(input, 2);
+    }
+
+    private static int Solve(string[] input, int part)
+    {
+        var operations = input[0].Split(',').ToList();
+        if (part == 1)
+        {
+            return operations.Sum(GetHash);
+        }
+
+        var boxes = Enumerable.Repeat(0, 256).Select(_ => new List<(string Lens, int FocalLength)>()).ToArray();
+        foreach (var operation in operations)
+        {
+            ProcessOperation(boxes, operation);
+        }
+
+        return boxes.Select((x, i) => x.Select((y, j) => (i + 1) * (j + 1) * y.FocalLength).Sum()).Sum();
+    }
+
+    private static void ProcessOperation(List<(string Label, int FocalLength)>[] boxes, string operation)
+    {
+        var label = operation.Split('-', '=')[0];
+        var boxIndex = GetHash(label);
+        var lensIndex = boxes[boxIndex].FindIndex(x => x.Label == label);
+
+        if (operation.Contains('='))
+        {
+            var focalLength = int.Parse(operation.Split('-', '=')[1]);
+            if (lensIndex == -1)
+            {
+                boxes[boxIndex].Add((label, focalLength));
+            }
+            else
+            {
+                boxes[boxIndex][lensIndex] = (label, focalLength);
+            }
+        }
+        else
+        {
+            if (lensIndex != -1)
+            {
+                boxes[boxIndex].RemoveAt(lensIndex);
+            }
+        }
+    }
+
+    private static int GetHash(string str)
+    {
+        return str.Aggregate(0, (x, y) => (x + y) * 17 % 256);
     }
 }
--- Tests/Tests/2023/Day15Test.cs
+++ Tests/Tests/2023/Day15Test.cs
@@ -9,13 +9,13 @@ namespace Tests.Tests._2023;
 [TestFixture]
 public class Day15Test
 {
-    private readonly string[] _sampleInput = @"".Split(Environment.NewLine).ToArray();
+    private readonly string[] _sampleInput = @"rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7".Split(Environment.NewLine).ToArray();
 
     private Day15 GetInstance() => new();
 
     [Test]
-    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(0);
+    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(1320);
 
     [Test]
-    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(0);
+    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(145);
 }
