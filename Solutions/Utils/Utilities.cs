4113743b4815f57add4d40528186a62313aabb65
onurjadermark <onurjadermark@gmail.com>
2023-12-20 09:54:12 +0100

    Add solution for day 20

--- Solutions/Utils/Utilities.cs
+++ Solutions/Utils/Utilities.cs
@@ -18,4 +18,12 @@ public static class Utilities
     {
         return (Direction) (((int) direction + 2) % 4);
     }
+
+    public static long GCD(long a, long b) => b == 0 ? a : GCD(b, a % b);
+
+    public static long LCM(long a, long b) => a / GCD(a, b) * b;
+
+    public static long LCM(IEnumerable<long> values) => values.Aggregate(LCM);
+
+    public static long LCM(IEnumerable<int> values) => LCM(values.Select(x => (long) x));
 }
