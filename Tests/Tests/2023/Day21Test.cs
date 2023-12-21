7a888f67c5065e12fbaf8801408396c93cb60e86
onurjadermark <onurjadermark@gmail.com>
2023-12-21 11:04:39 +0100

    Add solution for day 21

--- Tests/Tests/2023/Day21Test.cs
+++ Tests/Tests/2023/Day21Test.cs
@@ -9,13 +9,23 @@ namespace Tests.Tests._2023;
 [TestFixture]
 public class Day21Test
 {
-    private readonly string[] _sampleInput = @"".Split(Environment.NewLine).ToArray();
+    private readonly string[] _sampleInput = @"...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........".Split(Environment.NewLine).ToArray();
 
     private Day21 GetInstance() => new();
 
     [Test]
-    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(0);
+    public void Part1() => GetInstance().Part1(_sampleInput).Should().Be(16);
 
     [Test]
-    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(0);
+    public void Part2() => GetInstance().Part2(_sampleInput).Should().Be(50);
 }
