1ffb76df2533a7a1a76327f0f114da68fc4d5021
onurjadermark <onurjadermark@gmail.com>
2024-12-01 06:53:20 +0100

    Add solution for day 1

--- Solutions/Solutions/2024/Day01.cs
+++ Solutions/Solutions/2024/Day01.cs
@@ -4,16 +4,22 @@ public class Day01
 {
     public int Part1(string[] input)
     {
-        return Solve(input, 1);
+        var (numbers1, numbers2) = ParseInput(input);
+        numbers1.Sort();
+        numbers2.Sort();
+        return numbers1.Zip(numbers2, (x, y) => Math.Abs(x - y)).Sum();
     }
 
     public int Part2(string[] input)
     {
-        return Solve(input, 2);
+        var (numbers1, numbers2) = ParseInput(input);
+        return numbers1.Select(x => x * numbers2.Count(y => y == x)).Sum();
     }
 
-    private int Solve(string[] input, int part)
+    private static (List<int>, List<int>) ParseInput(string[] input)
     {
-        return 0;
+        var numbers1 = input.Select(x => int.Parse(x.Split()[0])).ToList();
+        var numbers2 = input.Select(x => int.Parse(x.Split()[3])).ToList();
+        return (numbers1, numbers2);
     }
 }
--- Tests/Tests/2024/Day01Test.cs
+++ Tests/Tests/2024/Day01Test.cs
@@ -9,13 +9,18 @@ namespace Tests.Tests._2024;
 [TestFixture]
 public class Day01Test
 {
-    private readonly string[] _sampleInput1 = @"".Split(Environment.NewLine).ToArray();
+    private readonly string[] _sampleInput1 = @"3   4
+4   3
+2   5
+1   3
+3   9
+3   3".Split(Environment.NewLine).ToArray();
 
     private Day01 GetInstance() => new();
 
     [Test]
-    public void Part1() => GetInstance().Part1(_sampleInput1).Should().Be(0);
+    public void Part1() => GetInstance().Part1(_sampleInput1).Should().Be(11);
 
     [Test]
-    public void Part2() => GetInstance().Part2(_sampleInput1).Should().Be(0);
+    public void Part2() => GetInstance().Part2(_sampleInput1).Should().Be(31);
 }
